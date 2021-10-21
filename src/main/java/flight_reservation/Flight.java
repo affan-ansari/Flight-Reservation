8c354089fb9796d435dbadf752f38151e413467b
Affan Arif <affanarif74@gmail.com>
2021-10-22 00:43:47 +0500

    First Commit

---
+++ src/main/java/flight_reservation/Flight.java
@@ -0,0 +1,16 @@
+package flight_reservation;
+
+import java.util.*;
+import java.time.*;
+
+public class Flight {
+	String source;
+	String destination;	
+	LocalDateTime departureDateTime;
+	LocalDateTime arrivalDateTime;
+	
+	public Flight() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/flight_reservation/Main.java
@@ -0,0 +1,10 @@
+package flight_reservation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
